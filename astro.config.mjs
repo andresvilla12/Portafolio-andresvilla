f4649b354c49a06eb84d809728a0ed4fa3c43b6c
andresvilla12 <villacalderon68@gmail.com>
2026-06-12 14:41:45 -0500

    feat: portafolio personal - andrés villa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
+import { fileURLToPath } from 'url'
+import path from 'path'
 
-// https://astro.build/config
-export default defineConfig({});
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://porfolio.dev/',
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  }
+})
