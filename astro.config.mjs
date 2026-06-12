5ed63c2e95b8d16bad4abe99a830587718c2fa7b
andresvilla12 <villacalderon68@gmail.com>
2026-06-12 16:13:53 -0500

    fix: corregir typo del adaptador Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,5 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-import robotsTxt from "astro-robots-txt"
-import { fileURLToPath } from 'url'
-import path from 'path'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
+import vercel from '@astrojs/vercel/static';
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/',
-  vite: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src')
-      }
-    }
-  }
-})
+  output: 'static',   // o 'server' si necesitas SSR
+  adapter: vercel(),
+});
