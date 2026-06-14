bd448926c409c8b1839a0016ed1213576a920e2e
andresvilla12 <villacalderon68@gmail.com>
2026-06-13 20:06:44 -0500

    fix: agregar adaptador Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
-import vercel from '@astrojs/vercel/static';
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
+import vercel from "@astrojs/vercel"
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-  output: 'static',   // o 'server' si necesitas SSR
+  output: "static",
   adapter: vercel(),
-});
+  integrations: [tailwind(), robotsTxt()],
+  site: 'https://porfolio.dev/',
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  }
+})
